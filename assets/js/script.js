7383f747e0a1379d17b8f0f4c57bd82f73c25b1e
Carlos Henrique <rapaiva21@gmail.com>
2021-10-28 13:48:12 -0300

    JS adicionando os dados na lista de acordo com o CSS... tudo em JS e feito antes de fazer o curso... sozinho com o que sei

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,13 @@ formButton.addEventListener("click" , function(event){
   if (form.value.length == 0){
     alert('Favor inserir os dados no campo para adicionar o item')
   } else {
-    
+    var list = document.querySelector('ul.list')
+    var task = document.createElement('li')
+    task.classList.add('task')
+    var content = document.createElement('p')
+    content.innerText=form.value
+    list.appendChild(content)
+    content.classList.add('content','task')
   }
-
+  form.value = ''
 })
