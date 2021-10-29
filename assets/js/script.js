8771bd356985b30eac9f24291d37c3c750976222
Carlos Henrique <rapaiva21@gmail.com>
2021-10-29 12:40:11 -0300

    Acompanhamento do curos de DOM da ALura. Fim da terceira parte que mostra como adicionar item na lista usado function arrow e data-atributte

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
-var formButton = document.querySelector('.form-button')
-formButton.addEventListener("click" , function(event){
-  event.preventDefault()
-  console.log('teste')
-  var form = document.querySelector('.form-input')
-  console.log(form.value)
-  if (form.value.length == 0){
-    alert('Favor inserir os dados no campo para adicionar o item')
-  } else {
-    var list = document.querySelector('ul.list')
-    var task = document.createElement('li')
-    task.classList.add('task')
-    var content = document.createElement('p')
-    content.innerText=form.value
-    list.appendChild(content)
-    content.classList.add('content','task')
-  }
-  form.value = ''
-})
+const criarTarefa = (evento) => {
+
+  evento.preventDefault()
+
+  const lista = document.querySelector('[data-list]')
+  const input = document.querySelector('[data-form-input]')
+  const valor = input.value
+
+  const tarefa = document.createElement('li')
+  tarefa.classList.add('task')
+
+  const conteudo = `<p class="content">${valor}</p>`
+  tarefa.innerHTML = conteudo
+
+  lista.appendChild(tarefa)
+  input.value = " "
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+novaTarefa.addEventListener('click', criarTarefa)
